1f838282bd08c695e62bb2de55577f19f14fb456
Pulkit Goyal <pulkitcgoyal@gmail.com>
2024-05-06 23:12:06 +0530

    add vercel web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivateRoutes from "./components/PrivateRoutes";
 import { AuthProvider } from "./utils/AuthContext";
+import { Analytics } from '@vercel/analytics/react';
+
 
 import Room from "./pages/Room";
 import LoginPage from "./pages/LoginPage";
@@ -11,6 +13,8 @@ function App() {
 	return (
 		<Router>
 			<AuthProvider>
+			<Analytics />
+
 				<Routes>
 					<Route path="/login" element={<LoginPage />} />
 					<Route path="/register" element={<RegisterPage />} />
